0b7e30727b88632a977a469409b79ca74d68c258
Katerina Nagikh <katerina@a0a0.ru>
2020-01-18 23:22:03 +0700

    дизайн приложения

--- app/src/main/java/com/example/otusfirstapp/DetailActivity.kt
+++ app/src/main/java/com/example/otusfirstapp/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.example.otusfirstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DetailActivity : AppCompatActivity() {
 
@@ -9,4 +11,20 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
     }
+
+    /*fun onClickinvite(view: View?) {
+        val textMessage = "Our message"
+        val sendIntent = Intent()
+        sendIntent.action = Intent.ACTION_SEND
+        sendIntent.putExtra(Intent.EXTRA_TEXT, textMessage)
+        sendIntent.type = "text/plain"
+        val title = resources.getString(R.string.chooser_title)
+        // Создаем Intent для отображения диалога выбора.
+        val chooser = Intent.createChooser(sendIntent, title)
+        // Проверяем, что intent может быть успешно обработан
+        sendIntent.resolveActivity(packageManager)?.let {
+                startActivity(chooser)
+            }
+        }
+    }*/
 }
--- app/src/main/java/com/example/otusfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/otusfirstapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.otusfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+
+    /*fun onClickfirstButton(view: View?) {
+        val Podrobnee = Toast.makeText(this, "Hello world!", Toast.LENGTH_SHORT)
+
+        Podrobnee.show()
+    }
+
+    fun onClicksecondButton(view: View?) {
+        val countString = textView.text.toString()
+        var count: Int = Integer.parseInt(countString)
+        count++
+        textView.text = count.toString()
+    }
+*/
+    fun onClickButton(view: View?) {
+
+        val textId = when (view?.id) {
+            R.id.button -> R.id.textView
+            R.id.button2 -> R.id.textView2
+            R.id.button3 -> R.id.textView3
+            else -> R.id.textView
+        }
+
+        val textView = findViewById<TextView>( textId )
+        textView.setTextColor(resources.getColor(R.color.colorAccent))
+
+
+        val ClickButtonIntent = Intent(this, DetailActivity::class.java)
+        startActivity(ClickButtonIntent)
+    }
 }
+
+
