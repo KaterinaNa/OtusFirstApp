7bc568d4f7c58f163167416e50f882113f76eb18
Katerina Nagikh <katerina@a0a0.ru>
2020-02-15 23:11:28 +0700

    реалтзована работа кнопки Детали в избранном

--- app/src/main/java/com/example/otusfirstapp/FavoritesActivity.kt
+++ app/src/main/java/com/example/otusfirstapp/FavoritesActivity.kt
@@ -1,5 +1,6 @@
 package com.example.otusfirstapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,27 +18,32 @@ class FavoritesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val addPosterButton = findViewById<Button>(R.id.addPoster)
         addPosterButton.setVisibility(View.VISIBLE)
         val delPosterButton = findViewById<Button>(R.id.delPoster)
         delPosterButton.setVisibility(View.VISIBLE)
-
 
         initRecyclerLike()
     }
 
     fun initRecyclerLike() {
         var likedFilms = ArrayList<Film>(items.filter { it.like })
+        val detailsListener = { id: Int ->
+            val openDetailsIntent = Intent(this, DetailActivity::class.java)
+            openDetailsIntent.putExtra("FilmId", id)
+            startActivityForResult(openDetailsIntent, REQUEST_CODE)
+        }
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.layoutManager = layoutManager
         val inflater = LayoutInflater.from(this)
-        recyclerView.adapter = PosterAdapter(inflater, likedFilms, {}, {})
+        recyclerView.adapter = PosterAdapter(inflater, likedFilms, {}, detailsListener)
 
         val itemDecor = PosterItemDecoration(this, DividerItemDecoration.VERTICAL)
         itemDecor.setDrawable(getDrawable(R.drawable.myline)!!)
         recyclerView.addItemDecoration(itemDecor)
-
 
         findViewById<View>(R.id.addPoster).setOnClickListener {
             likedFilms.add(Film("Ла ла ленд", R.drawable.lalaland, "Это история любви старлетки, которая между прослушиваниями подает кофе состоявшимся кинозвездам, и фанатичного джазового музыканта, вынужденного подрабатывать в заштатных барах. Но пришедший к влюбленным успех начинает подтачивать их отношения.", true))
